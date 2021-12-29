da491952b5e9e187d96e17f84d5c0bc353c39d25
Rodrigo Miguel <rodrigosmig@gmail.com>
2021-12-29 13:01:15 -0300

    adjustment in next link

--- src/components/Layout/Header/NavAddItem.tsx
+++ src/components/Layout/Header/NavAddItem.tsx
@@ -1,13 +1,12 @@
 import {  
-  Box,
-  Stack,
-  Text,
   Flex,
+  LinkBox,
+  LinkOverlay,
+  Stack,
   useColorModeValue
 } from "@chakra-ui/react";
 import NextLink from 'next/link';
 import { ReactElement } from "react";
-import { Link } from "../../Link";
 
 interface NavAddItemProps {
   url: string;
@@ -19,35 +18,36 @@ export const NavAddItem = ({ icon, label, url }: NavAddItemProps) => {
   const bg = useColorModeValue('gray.50', 'gray.800');
 
   return (
-    <NextLink href={url} passHref >
-      <Link
-        href=""
-        role={'group'}
-        display={'block'}
-        p={2}
-        rounded={'md'}
-        _hover={{ bg: bg }}>
-        <Stack direction={'row'}>
-          <Flex
-            transition={'all .3s ease'}
-            _groupHover={{ color: 'pink.400' }}
-            align={'center'}
-            fontSize={['sm', "md", "md"]}
-          >
-            { icon }
-          </Flex>
+    <LinkBox>
+      <NextLink href={url} passHref >
+        <LinkOverlay
+          role={'group'}
+          display={'block'}
+          p={2}
+          rounded={'md'}
+          _hover={{ bg: bg }}>
+          <Stack direction={'row'}>
+            <Flex
+              transition={'all .3s ease'}
+              _groupHover={{ color: 'pink.400' }}
+              align={'center'}
+              fontSize={['sm', "md", "md"]}
+            >
+              { icon }
+            </Flex>
 
-          <Flex
-            transition={'all .3s ease'}
-            _groupHover={{ color: 'pink.400' }}
-            align={'center'}
-            
-            fontSize={['sm', "md", "md"]}
-          >
-            { label }
-          </Flex>
-        </Stack>
-      </Link>
-    </NextLink>
+            <Flex
+              transition={'all .3s ease'}
+              _groupHover={{ color: 'pink.400' }}
+              align={'center'}
+              
+              fontSize={['sm', "md", "md"]}
+            >
+              { label }
+            </Flex>
+          </Stack>
+        </LinkOverlay>
+      </NextLink>    
+    </LinkBox>
   )
 }
