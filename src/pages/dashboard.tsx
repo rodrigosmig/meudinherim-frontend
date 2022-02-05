7e2c0e46794a4d750ef6e38339be2b9d85d01330
Rodrigo Miguel <rodrigosmig@gmail.com>
2022-02-04 21:51:31 -0300

    Update dashboard.tsx

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -188,13 +188,3 @@ export default function Dashboard() {
     </>
   )
 }
-
-export const getServerSideProps = withSSRAuth(async (context) => {
-  const apiClient = setupApiClient(context);
-
-  const response = await apiClient.get('/auth/me');
-
-  return {
-    props: {}
-  }
-})
