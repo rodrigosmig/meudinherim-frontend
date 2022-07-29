6fdcf75bf145ebb105a3a04a0dd335cd6e86d799
Rodrigo Miguel <rodrigosmig@gmail.com>
2022-07-29 20:48:30 -0300

    Fixing build issues

--- src/services/ApiService/CategoryService.ts
+++ src/services/ApiService/CategoryService.ts
@@ -14,7 +14,7 @@ const apiClient = setupApiClient();
 export const categoryService = {
   list: (type: string, active: boolean, page: number, perPage: number): Promise<AxiosResponse<ICategoryResponse>> => apiClient.get(`/categories?type=${type}&active=${active}&page=${page}&per_page=${perPage}`),
   getAllByType: (type: CategoryType): Promise<AxiosResponse<ICategoryResponse>> => apiClient.get(`/categories?type=${type}&per_page=1000`),
-  listForForm: (): Promise<AxiosResponse<ICategoryForm>> => apiClient.get(`/categories?type=all`),
+  listForForm: (): Promise<AxiosResponse<ICategoryForm>> => apiClient.get(`/categories?form=true`),
   update: (values: ICategoryUpdateData): Promise<AxiosResponse<ICategory>> => apiClient.put(`/categories/${values.categoryId}`, values.data),
   delete: (id: number): Promise<AxiosResponse> => apiClient.delete(`/categories/${id}`),
   create: (values: ICategoryFormData): Promise<AxiosResponse<ICategory>> => apiClient.post(`/categories`, values)
